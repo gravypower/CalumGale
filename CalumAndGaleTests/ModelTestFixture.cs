0de66834116a4639d3883cc69c5f5fe6c76998d1
Aaron <aaron@gravypower.net>
2013-08-20 08:16:56 +0100

    updated db connection string
    fixed up some more name spaces

--- CalumAndGaleTests/ModelTestFixture.cs
+++ CalumAndGaleTests/ModelTestFixture.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using CalumGayleWeb.Models;
 using NUnit.Framework;
 
-namespace CalumAndGayleTests
+namespace CalumGayleTests
 {
     [TestFixture]
     public class ModelTestFixture
